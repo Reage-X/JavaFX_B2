83728528dfc68fe838247157f03a89ed330fd1c0
Reage-X <sergekshyvnyak@gmail>
2025-12-03 10:27:07 +0100

    Format `Joueur` class getters and setters to improve code readability

--- src/main/java/com/example/demo/Joueur.java
+++ src/main/java/com/example/demo/Joueur.java
@@ -32,20 +32,20 @@ public class Joueur extends Compte
         this.sprite_frame = 0;
     }
 
-    public int getX()                                   {return x;}
-    public int getY()                                   {return y;}
-    public int getDx()                                  {return dx;}
-    public int getDy()                                  {return dy;}
-    public int getScore_jeu()                           {return score_jeu;}
-    public int getNb_vies()                             {return nb_vies;}
-    public int getSprite_frame()                        {return sprite_frame;}
+    public int getX()                                   { return x; }
+    public int getY()                                   { return y; }
+    public int getDx()                                  { return dx; }
+    public int getDy()                                  { return dy; }
+    public int getScore_jeu()                           { return score_jeu; }
+    public int getNb_vies()                             { return nb_vies; }
+    public int getSprite_frame()                        { return sprite_frame; }
 
 
-    public void setX(int x)                             {this.x = x;}
-    public void setY(int y)                             {this.y = y;}
-    public void setDx(int dx)                           {this.dx = dx;}
-    public void setDy(int dy)                           {this.dy = dy;}
-    public void setScore(int score_jeu)                 {this.score_jeu = score_jeu;}
-    public void setNb_vies(int nb_vies)                 {this.nb_vies = nb_vies;}
-    public void setSprite_frame(int sprite_frame)       {this.sprite_frame = sprite_frame;}
+    public void setX(int x)                             { this.x = x; }
+    public void setY(int y)                             { this.y = y; }
+    public void setDx(int dx)                           { this.dx = dx; }
+    public void setDy(int dy)                           { this.dy = dy; }
+    public void setScore(int score_jeu)                 { this.score_jeu = score_jeu; }
+    public void setNb_vies(int nb_vies)                 { this.nb_vies = nb_vies; }
+    public void setSprite_frame(int sprite_frame)       { this.sprite_frame = sprite_frame; }
 }
