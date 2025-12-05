6f6e6f3dca30b51aa8cb093961bfc05495fa9236
Reage-X <sergekshyvnyak@gmail>
2025-12-05 23:44:55 +0100

    Add database connection setup and SQL query method

--- src/main/java/com/example/demo/Sql.java
+++ src/main/java/com/example/demo/Sql.java
@@ -1,5 +1,21 @@
 package com.example.demo;
+import java.util.ArrayList;
+import java.sql.*;
+
 
 public class Sql {
-    public Sql() {}
+    public static Compte getCompte(String nom, String prenom, Connection conn) throws SQLException {
+        String sqlEtu = "SELECT * FROM etudiants WHERE nom = ? AND prenom = ?";
+        Compte etu = null;
+    }
+}
+
+public static void main(String[] args) throws SQLException {
+    String url = "jdbc:mysql://localhost:3306/javafx-b2?createDatabaseIfNotExist=true";
+    String user = "root";
+    String pass = "";
+    //Compte compteTest = null
+    try (Connection conn = DriverManager.getConnection(url, user, pass)) {
+        System.out.println("Connexion à la BDD réussie.");
+    }
 }
