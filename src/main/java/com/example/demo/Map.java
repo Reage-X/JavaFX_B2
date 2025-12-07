11474552573bb0304c5f53aa35fbc31844db14b2
NicolasStuhlen <150047429+NicolasStuhlen@users.noreply.github.com>
2025-12-07 21:23:40 +0100

    Update Map.java

--- src/main/java/com/example/demo/Map.java
+++ src/main/java/com/example/demo/Map.java
@@ -33,25 +33,25 @@ public class Map {
     private void fillWithOrbs() {
         for (int y = 0; y < hauteur; y++) {
             for (int x = 0; x < longueur; x++) {
-                map[y][x] = ORB;  // Par défaut : ORBE
+                map[x][y] = ORB;  // Par défaut : ORBE
             }
         }
     }
 
     /** Ajoute un mur qui remplace l'orbe */
     public void addWall(int x, int y) {
-        map[y][x] = WALL;
+        map[x][y] = WALL;
     }
 
     /** Récupère le contenu d'une case */
     public char getCell(int x, int y) {
-        return map[y][x];
+        return map[x][y];
     }
 
     /** Dépose une case vide quand le joueur ramasse une orbe */
     public void collectOrbAt(int x, int y) {
-        if (map[y][x] == ORB) {
-            map[y][x] = EMPTY;  // devient vide
+        if (map[x][y] == ORB) {
+            map[x][y] = EMPTY;  // devient vide
         }
     }
 
@@ -63,7 +63,7 @@ public class Map {
                 if (joueur.getX() == x && joueur.getY() == y) {
                     System.out.print('P');
                 } else {
-                    System.out.print(map[y][x]);
+                    System.out.print(map[x][y]);
                 }
 
             }
