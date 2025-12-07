fff3de0f03a2b8586e7aeac4db543b49ea412575
Reage-X <sergekshyvnyak@gmail>
2025-12-07 16:40:05 +0100

    Rename `Joueur` class to `Entity`, update its usage in `Compte`, and add methods for score management.

--- src/main/java/com/example/demo/Compte.java
+++ src/main/java/com/example/demo/Compte.java
@@ -1,13 +1,13 @@
 package com.example.demo;
-
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Compte
 {
     private String user_name;
     private String MDP;
     private ArrayList<Integer> score;
-    private Joueur joueur;
+    private Entity joueur;
 
     public Compte(String user_name, String MDP)
     {
@@ -15,17 +15,31 @@ public class Compte
         this.MDP = MDP;
     }
 
-    public Compte() {
-    }
+    public Compte() {}
 
     public String getUser_name()                            { return user_name; }
     public String getMDP()                                  { return MDP; }
     public ArrayList<Integer> getScore()                    { return score; }
-    public Joueur getJoueur()                               { return joueur; }
+    public Integer getScore(int index) {
+        if (score != null && index >= 0 && index < score.size()) {
+            return score.get(index);
+        }
+        return null;
+    }
+    public Entity getJoueur()                               { return joueur; }
+
 
     public void setUser_name(String user_name)              { this.user_name = user_name; }
     public void setMDP(String MDP)                          { this.MDP = MDP; }
     public void setScore(ArrayList<Integer> score)          { this.score = score; }
-    public void setJoueur(Joueur joueur)                    { this.joueur = joueur; }
-
+    public void addScore(Integer nouveauScore) {
+        if (this.score == null) {
+            this.score = new ArrayList<>();
+        }
+        if (nouveauScore >= 0) {
+            this.score.add(nouveauScore);
+            this.score.sort(Comparator.reverseOrder());
+        }
+    }
+    public void setJoueur(Entity joueur)                    { this.joueur = joueur; }
 }
